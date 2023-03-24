04b250b9c9660f992f951f64396caaef0f1cabcf
DurjoyAcharya <da-durjoy@outlook.com>
2023-03-24 19:53:45 +0600

    Somke Testing

--- src/test/java/com/app/springbootwebapplicationtesting/SmokeTest.java
+++ src/test/java/com/app/springbootwebapplicationtesting/SmokeTest.java
@@ -2,13 +2,24 @@ package com.app.springbootwebapplicationtesting;
 
 
 import com.app.springbootwebapplicationtesting.Controller.HomeController;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 
 @SpringBootTest
 public class SmokeTest {
     @Autowired
     private HomeController homeController;
+
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(homeController).isNotNull();
+    }
+
 
 
 
